b266fb5b09dbbf0a075698c0a68b5048d713952f
Dario Fervenza <dario.fervenza.garcia@gmail.com>
2025-07-12 15:58:05 +0200

    started section 5, added untracked files

--- Section_5/if_let/src/main.rs
+++ Section_5/if_let/src/main.rs
@@ -8,10 +8,18 @@ fn main() {
 
     // if let are equivalents to a match with only one option, but it can contain an else to allow other patterns
     let dog2 = Some(Pet::Dog);
-    if let Some(Pet::Dog) = dog2 {
+    if let Some(Pet::Dog) = dog2 { // with if let we dont use ==
         println!("The animal is a dog");
-    } else {
+    } else {  // we could use else if also
         println!("Its not a dog");
+    }
+
+    let mut stack = Vec::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    while let Some(top) = stack.pop() {  // while there is some number, we will iterate
+        println!("top is {}", top)
     }
 
 }
